6eaca5de87fd182ba57a00b03feabdcff5a899a1
Adam Smith <adam@roadlyfe.com>
2022-11-29 13:07:39 -0800

    created Projects component with inital styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import Hero from '../components/Hero'
 import About from '../components/About'
 import WorkExperience from '../components/WorkExperience'
 import Skills from '../components/Skills'
+import Projects from '../components/Projects'
 
 const Home: NextPage = () => {
   return (
@@ -30,7 +31,9 @@ const Home: NextPage = () => {
       <Skills />
     </section>
       {/* Projects */}
-
+      <section id='projects' className='snap-start' >
+    <Projects />
+    </section>
       {/*  Contact Me */}
 
     </div>
