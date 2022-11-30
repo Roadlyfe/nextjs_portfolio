e4a5db2b12409e3399fe90d655f2a437ff1dc910
Adam Smith <adam@roadlyfe.com>
2022-11-30 11:32:01 -0800

    install and implemment hero icons

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-
+import React from 'react';
+import { PhoneIcon, MapPinIcon, EnvelopeIcon } from '@heroicons/react/24/solid';
 type Props = {}
 
 function ContactMe({}: Props) {
@@ -8,11 +8,18 @@ function ContactMe({}: Props) {
         <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
         Contact
         </h3>
-        <div>
+        <div className='flex flex-col space-y-10'>
             <h4 className='text-4xl font-semibold text-center'>
                 I've got just what you need. {" "}
                 <span className='decoration-[#F7AB0A]/50 underline'>Let's Talk</span>
             </h4>
+            <div>
+            <div className='flex items-center space-x-5 justify-center'>
+                {/* come back here to try diff animations for phone icon */}
+                <PhoneIcon className='text-[#F7AB0A] h-7 w-7 animate-pulse'/>
+                <p className='text-2xl'>503-975-6868</p>
+            </div>
+            </div>
         </div>
     </div>
   )
