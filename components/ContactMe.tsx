52538a308daa5e86f1328c40ef377a230e115ee3
Adam Smith <adam@roadlyfe.com>
2022-11-30 12:12:17 -0800

    install and implement react-hook-form in contact section

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -1,28 +1,59 @@
 import React from 'react';
 import { PhoneIcon, MapPinIcon, EnvelopeIcon } from '@heroicons/react/24/solid';
+import { useForm, SubmitHandler } from "react-hook-form";
+
 type Props = {}
 
-function ContactMe({}: Props) {
-  return (
-    <div className='h-screen flex relative flex-col text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
-        <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
-        Contact
-        </h3>
-        <div className='flex flex-col space-y-10'>
-            <h4 className='text-4xl font-semibold text-center'>
-                I've got just what you need. {" "}
-                <span className='decoration-[#F7AB0A]/50 underline'>Let's Talk</span>
-            </h4>
-            <div>
-            <div className='flex items-center space-x-5 justify-center'>
-                {/* come back here to try diff animations for phone icon */}
-                <PhoneIcon className='text-[#F7AB0A] h-7 w-7 animate-pulse'/>
-                <p className='text-2xl'>503-975-6868</p>
-            </div>
+type Inputs = {
+    example: string,
+    exampleRequired: string,
+  };
+
+function ContactMe({ }: Props) {
+    const { register, handleSubmit, watch, formState: { errors } } = useForm<Inputs>();
+    const onSubmit: SubmitHandler<Inputs> = data => console.log(data);
+
+    return (
+        <div className='h-screen flex relative flex-col text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
+            <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
+                Contact
+            </h3>
+            <div className='flex flex-col space-y-10'>
+                <h4 className='text-4xl font-semibold text-center'>
+                    I've got just what you need. {" "}
+                    <span className='decoration-[#F7AB0A]/50 underline'>Let's Talk</span>
+                </h4>
+                <div className='space-y-10'>
+                    <div className='flex items-center space-x-5 justify-center'>
+                        {/* come back here to try diff animations for phone icon */}
+                        <PhoneIcon className='text-[#F7AB0A] h-7 w-7 animate-pulse' />
+                        <p className='text-2xl'>503-975-6868</p>
+                    </div>
+                    <div className='flex items-center space-x-5 justify-center'>
+                        {/* come back here to try diff animations for phone icon */}
+                        <EnvelopeIcon className='text-[#F7AB0A] h-7 w-7 animate-pulse' />
+                        <p className='text-2xl'>adamsmithetc@gmail.com</p>
+                    </div>
+                    <div className='flex items-center space-x-5 justify-center'>
+                        {/* come back here to try diff animations for phone icon */}
+                        <MapPinIcon className='text-[#F7AB0A] h-7 w-7 animate-pulse' />
+                        <p className='text-2xl'>503-975-6868</p>
+                    </div>
+                </div>
+                <form className='flex flex-col space-y-2 w-fit mx-auto'>
+                    <div className='flex space-x-2'>
+                        <input placeholder='Name' className='contactInput' type="text" />
+                        <input placeholder='Email' className='contactInput' type="email" />
+                    </div>
+                    <input placeholder='Subject' className='contactInput' type="text" />
+                    <textarea placeholder='Message' className='contactInput' />
+                    <button type='submit' className='bg-[#F7AB0A] py-5 px-10 rounded-md text-black font-bold text-lg'>
+                        Submit
+                        </button>
+                </form>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default ContactMe
