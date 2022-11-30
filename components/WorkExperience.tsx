829a121fab3590f984b452a1922e739fe14f5ac7
Adam Smith <adam@roadlyfe.com>
2022-11-30 14:25:15 -0800

    styled scrollbars with tailwind scrollbar plugin

--- components/WorkExperience.tsx
+++ components/WorkExperience.tsx
@@ -14,7 +14,7 @@ function WorkExperience({}: Props) {
         <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
             Experience
         </h3>
-        <div className='w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory'>
+        <div className='w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#f7ab0a]/80'>
             <ExperienceCard />
             <ExperienceCard />
             <ExperienceCard />
