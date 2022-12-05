c53de5f0285818048a00586a101d414918d49319
Adam Smith <adam@roadlyfe.com>
2022-12-05 10:54:37 -0800

    scaffolding complete, starting santiy.io backend

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -25,15 +25,15 @@ function ContactMe({ }: Props) {
 
     return (
         <div className='h-screen flex relative flex-col text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
-            <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
+            <h3 className='absolute top-4 uppercase tracking-[20px] text-gray-500 text-2xl'>
                 Contact
             </h3>
-            <div className='flex flex-col space-y-10'>
-                <h4 className='text-4xl font-semibold text-center'>
+            <div className='flex flex-col space-y-1 pt-10'>
+                <h4 className='text-4xl font-semibold text-center pb-5'>
                     I've got just what you need. {" "}
                     <span className='decoration-[#F7AB0A]/50 underline'>Let's Talk</span>
                 </h4>
-                <div className='space-y-10'>
+                <div className='space-y-5'>
                     <div className='flex items-center space-x-5 justify-center'>
                         {/* come back here to try diff animations for phone icon */}
                         <PhoneIcon className='text-[#F7AB0A] h-7 w-7 animate-pulse' />
@@ -57,7 +57,7 @@ function ContactMe({ }: Props) {
                     </div>
                     <input {...register('subject')} placeholder='Subject' className='contactInput' type="text" />
                     <textarea {...register('message')} placeholder='Message' className='contactInput' />
-                    <button type='submit' className='bg-[#F7AB0A] py-5 px-10 rounded-md text-black font-bold text-lg'>
+                    <button type='submit' className='bg-[#00f5d4] py-5 px-10 rounded-md text-black font-bold text-lg'>
                         Submit
                     </button>
                 </form>
--- components/Header.tsx
+++ components/Header.tsx
@@ -8,29 +8,29 @@ type Props = {}
 export default function Header({ }: Props) {
     return (
         <header className='sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center'>
-            <motion.div 
-            initial={{ 
-                x: -500, 
-                opacity: 0,
-                scale: 0.5,
-             }}
-             animate={{
-                x: 0,
-                opacity: 1,
-                scale: 1,
-             }}
-             transition={{
-                duration: 1.5,
-             }}
-            className='flex flex-row items-center'>
+            <motion.div
+                initial={{
+                    x: -500,
+                    opacity: 0,
+                    scale: 0.5,
+                }}
+                animate={{
+                    x: 0,
+                    opacity: 1,
+                    scale: 1,
+                }}
+                transition={{
+                    duration: 1.5,
+                }}
+                className='flex flex-row items-center'>
                 {/* Social Icons */}
                 <SocialIcon
-                    url='https://www.youtube.com/channel/UCUcr2WcJaUQ8nw_T9RZrjaw'
+                    url='https://github.com/Roadlyfe'
                     fgColor='gray'
                     bgColor='transparent'
                 />
                 <SocialIcon
-                    url='https://www.youtube.com/channel/UCUcr2WcJaUQ8nw_T9RZrjaw'
+                    url='https://www.instagram.com/roadlyfe/'
                     fgColor='gray'
                     bgColor='transparent'
                 />
@@ -42,19 +42,19 @@ export default function Header({ }: Props) {
             </motion.div>
 
             <motion.div
-            initial={{
-                x: 500,
-                opacity: 0,
-                scale: 1,
-            }}
-            animate={{
-                x: 0,
-                opacity: 1,
-                scale: 1,
-            }}
-            transition={{ duration: 1.5 }}
-            className='flex flex-row items-center text-gray-300 cursor-pointer'>
-                 <SocialIcon
+                initial={{
+                    x: 500,
+                    opacity: 0,
+                    scale: 1,
+                }}
+                animate={{
+                    x: 0,
+                    opacity: 1,
+                    scale: 1,
+                }}
+                transition={{ duration: 1.5 }}
+                className='flex flex-row items-center text-gray-300 cursor-pointer'>
+                <SocialIcon
                     className='cursor-pointer'
                     network='email'
                     fgColor='gray'
