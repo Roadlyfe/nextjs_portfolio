39bc6adb6bc63ee19d2bc053baac85af2d02a434
Adam Smith <adam@roadlyfe.com>
2022-12-07 11:35:34 -0800

    added a thumb to the bottom that returns to hero

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -24,12 +24,12 @@ function ContactMe({ }: Props) {
     };
 
     return (
-        <div className='h-screen flex relative flex-col text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
+        <div className='h-screen flex relative flex-col text-center md:text-left md:flex-row max-w-7xl px-10 pb-10 justify-evenly mx-auto items-center'>
             <h3 className='absolute top-4 uppercase tracking-[20px] text-gray-500 text-2xl'>
                 Contact
             </h3>
-            <div className='flex flex-col space-y-1 pt-10'>
-                <h4 className='text-4xl font-semibold text-center pb-5'>
+            <div className='flex flex-col space-y-1 pt-20'>
+                <h4 className='text-3xl font-semibold text-center pb-5'>
                     I've got just what you need. {" "}
                     <span className='decoration-[#F7AB0A]/50 underline'>Let's Talk</span>
                 </h4>
