03bbee4e9da24a61a08614ecef55afe1b93439ae
Adam Smith <adamsmith@Adams-Mac-mini.local>
2022-12-12 10:57:22 -0800

    fixed image on about section

--- components/About.tsx
+++ components/About.tsx
@@ -27,7 +27,7 @@ function About({}: Props) {
         viewport={{ once: true }}
         src='https://scontent-sjc3-1.xx.fbcdn.net/v/t31.18172-8/30072846_10214156273634649_6995639784459986368_o.jpg?_nc_cat=110&ccb=1-7&_nc_sid=730e14&_nc_ohc=A-kV7kUVqsoAX_vLMOP&tn=LndM5tPVKDfZJ3-n&_nc_ht=scontent-sjc3-1.xx&oh=00_AfBTDNi_Rv0lQE4tDmhRMbLz11CquCmxdedCKmq5qyrHDg&oe=639F805E'
         //come back here to adjust about picture. 
-        className='-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-95 xl:object-scale-down xl:w-500'
+        className='-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-95 xl:object-scale-down xl:h-96'
         />
 
         <div className='space-y-10 px-0 md:px-10'>
